f2a30dede9f82290cd1fcff315b952f3f49eae47
ahmed-at1802794 <aterchoun2001@hotmail.com>
2023-02-03 17:52:52 +0300

    worked on game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,24 +1,55 @@
 'use strict';
 
-const secretNumber = Math.floor(Math.random() * 21);
+let secretNumber = Math.floor(Math.random() * 21);
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
 
+    //when there is no input
     if (!guess) {
-        console.log('Number Detected');
+        document.querySelector('.message').textContent = 'No Number';
     }
-    if (guess === secretNumber) {
+
+    //when the guess is correct
+    else if (guess === secretNumber) {
+        document.querySelector('.number').textContent = secretNumber;
         document.querySelector('.message').textContent = 'Correct Number';
         document.querySelector('.highscore').textContent = score;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+        //when the guess is lower
     } else if (guess < secretNumber) {
-        document.querySelector('.message').textContent = 'Too Low';
-        score--;
-        document.querySelector('.score').textContent = score;
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too Low';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'Game Over';
+            document.querySelector('.score').textContent = 0;
+        }
+        //when the guess is higher
     } else if (guess > secretNumber) {
-        document.querySelector('.message').textContent = 'Too high';
-        score--;
-        document.querySelector('.score').textContent = score;
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too High';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'Game Over';
+            document.querySelector('.score').textContent = 0;
+        }
     }
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secretNumber = Math.floor(Math.random() * 21);
+
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
 });
